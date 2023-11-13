9ee0c43b4ebdd714857a27e8bba9d7462022082e
demetgas <demet_gas@hotmail.com>
2023-11-13 20:40:29 +0100

    Update shopContext.jsx

--- src/context/shopContext.jsx
+++ src/context/shopContext.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { createContext, useState } from "react";
+import { PRODUCTS } from "../products";
 
-export const ShopContext = () => {
-  return <div>shopContext</div>;
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.length; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
+
+export const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState();
+  return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
 };
