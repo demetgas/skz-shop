5aa0cc6cf3f8af6ae5d4cde00bda16ebdf3b8e42
demetgas <demet_gas@hotmail.com>
2023-11-13 19:10:38 +0100

    pictures added

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>SKZ Shop</h1>
+      </div>
+      <div className="products">
+        
+      </div>
+    </div>
+  );
 };
---
+++ src/products.js
@@ -0,0 +1,59 @@
+import p1 from "./assets/1.png";
+import p2 from "./assets/2.png";
+import p3 from "./assets/3.png";
+import p4 from "./assets/4.png";
+import p5 from "./assets/5.png";
+import p6 from "./assets/6.png";
+import p7 from "./assets/7.png";
+import p8 from "./assets/8.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Tshirt",
+    price: 17.99,
+    productImage: p1,
+  },
+  {
+    id: 2,
+    productName: "Tshirt",
+    price: 17.99,
+    productImage: p2,
+  },
+  {
+    id: 3,
+    productName: "Tshirt",
+    price: 17.99,
+    productImage: p3,
+  },
+  {
+    id: 4,
+    productName: "Tshirt",
+    price: 17.99,
+    productImage: p4,
+  },
+  {
+    id: 5,
+    productName: "Tshirt",
+    price: 17.99,
+    productImage: p5,
+  },
+  {
+    id: 6,
+    productName: "Tshirt",
+    price: 17.99,
+    productImage: p6,
+  },
+  {
+    id: 7,
+    productName: "Tshirt",
+    price: 17.99,
+    productImage: p7,
+  },
+  {
+    id: 8,
+    productName: "Tshirt",
+    price: 17.99,
+    productImage: p8,
+  },
+];
