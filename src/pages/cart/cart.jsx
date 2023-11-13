15ee62849e180fc1ba562c674d5c21ef5844c48d
demetgas <demet_gas@hotmail.com>
2023-11-13 21:27:02 +0100

    cart items created

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../products";
+import { ShopContext } from "../../context/shopContext";
+import { CartItem } from "./cartItem";
+import './cart.css'
 
 export const Cart = () => {
+  const { cartItems } = useContext(ShopContext);
+
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="cart">
+      <div>
+        <h1> Your Items</h1>
+      </div>
+      <div className="cartItems">{PRODUCTS.map((product) => {
+        if(cartItems[product.id] !==0){
+          return <CartItem data={product}/>
+        }
+      })}</div>
+    </div>
+  );
+};
