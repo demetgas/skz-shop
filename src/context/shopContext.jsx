db59495ad6c6d6977cc9934992a3febbbd6e23c2
demetgas <demet_gas@hotmail.com>
2023-11-13 20:47:06 +0100

    Update shopContext.jsx

--- src/context/shopContext.jsx
+++ src/context/shopContext.jsx
@@ -21,5 +21,11 @@ export const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
+  const contextValue = { cartItems, addToCart, removeFromCart };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
 };
