ead0aa4529d310cfc6717751e970fde37af33f80
demetgas <demet_gas@hotmail.com>
2023-11-13 21:38:52 +0100

    Update cartItem.jsx

--- src/pages/cart/cartItem.jsx
+++ src/pages/cart/cartItem.jsx
@@ -3,7 +3,7 @@ import { ShopContext } from "../../context/shopContext";
 
 export const CartItem = (props) => {
   const { id, productName, price, productImage } = props.data;
-  const { cartItems } = useContext(ShopContext);
+  const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
 
   return (
     <div className="cartItem">
@@ -14,9 +14,9 @@ export const CartItem = (props) => {
         </p>
         <p>€{price}</p>
         <div className="countHandler">
-          <button> - </button>
+          <button onClick={()=>removeFromCart(id)}> - </button>
           <input value={cartItems[id]} />
-          <button> + </button>
+          <button onClick={()=>addToCart(id)}> + </button>
         </div>
       </div>
     </div>
