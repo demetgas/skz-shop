270e2a61f7a2a3cc7161dd6cb2d51e6ed8011fd1
demetgas <demet_gas@hotmail.com>
2023-11-13 18:36:36 +0100

    added css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import "./navbar.css";
 import { Link } from "react-router-dom";
-import {ShoppingCart} from "phosphor-react"; 
+import { ShoppingCart } from "phosphor-react";
 
 export const Navbar = () => {
+    
   return (
     <div className="navbar">
       <div className="links">
         <Link to="/"> Shop </Link>
-        <Link to="/cart"> <ShoppingCart size={32}/> </Link>
+        <Link to="/cart">
+          {" "}
+          <ShoppingCart size={28} />{" "}
+        </Link>
       </div>
     </div>
   );
