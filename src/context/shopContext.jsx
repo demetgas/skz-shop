de012c2963b7ce1670618f0c72b7d0cc5e6ea970
demetgas <demet_gas@hotmail.com>
2023-11-13 20:41:33 +0100

    Update shopContext.jsx

--- src/context/shopContext.jsx
+++ src/context/shopContext.jsx
@@ -5,13 +5,13 @@ export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
   let cart = {};
-  for (let i = 1; i < PRODUCTS.length+1; i++) {
+  for (let i = 1; i < PRODUCTS.length + 1; i++) {
     cart[i] = 0;
   }
   return cart;
 };
 
 export const ShopContextProvider = (props) => {
-  const [cartItems, setCartItems] = useState();
+  const [cartItems, setCartItems] = useState(getDefaultCart());
   return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
 };
