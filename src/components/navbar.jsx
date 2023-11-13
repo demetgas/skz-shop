992b96bb0b22eb6bd8484e28e824418c1ec05dc6
demetgas <demet_gas@hotmail.com>
2023-11-13 18:07:19 +0100

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="links">
+        <Link to="/"> Shop </Link>
+        <Link to="/cart"> Cart </Link>
+      </div>
+    </div>
+  );
 };
