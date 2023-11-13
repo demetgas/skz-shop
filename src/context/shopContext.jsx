dfe7ba65c9e651163ea5cba97ab3ec2b4c7f6cbd
demetgas <demet_gas@hotmail.com>
2023-11-13 21:54:42 +0100

    created total

--- src/context/shopContext.jsx
+++ src/context/shopContext.jsx
@@ -25,7 +25,18 @@ export const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
   };
 
-  const contextValue = { update, cartItems, addToCart, removeFromCart };
+  const total = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
+    }
+    return totalAmount;
+  };
+
+  const contextValue = { total, update, cartItems, addToCart, removeFromCart };
 
   return (
     <ShopContext.Provider value={contextValue}>
