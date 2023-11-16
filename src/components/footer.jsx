29ce215dfab6f81b1691ada533adc6626c7667ca
demetgas <demet_gas@hotmail.com>
2023-11-16 17:18:05 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,27 @@ export const Footer = () => {
   return (
     <div className="footer">
       <div className="footerLists">
-        <div className="footerList"></div>
+        <ul className="list">
+          <a href="" className="listTitle">
+            SKZ SHOP
+          </a>
+          <li className="listItems">Frequently Asked Questions</li>
+          <li className="listItems">Careers</li>
+        </ul>
+        <ul className="list">
+          <a href="" className="listTitle">
+            ABOUT US
+          </a>
+          <li className="listItems">Contact Us</li>
+          <li className="listItems">Sustainability</li>
+        </ul>
+        <ul className="list">
+          <a href="" className="listTitle">
+            HELP
+          </a>
+          <li className="listItems">Elite Membership</li>
+          <li className="listItems">Return</li>
+        </ul>
       </div>
     </div>
   );
