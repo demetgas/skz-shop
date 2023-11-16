bdefea52652ba4ee665eae9f398d361fe347ed73
demetgas <demet_gas@hotmail.com>
2023-11-16 18:01:28 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,21 +11,21 @@ export const Footer = () => {
           </a>
           <li className="listItems">Frequently Asked Questions</li>
           <li className="listItems">Careers</li>
-          <li className="listItems">Careers</li>
+          <li className="listItems">Reviews</li>
         </ul>
         <ul className="list">
           <a href="" className="listTitle">
             ABOUT US
           </a>
           <li className="listItems">Contact Us</li>
-          <li className="listItems">Careers</li>
+          <li className="listItems">Trems & Conditions</li>
           <li className="listItems">Sustainability</li>
         </ul>
         <ul className="list">
           <a href="" className="listTitle">
             HELP
           </a>
-          <li className="listItems">Careers</li>
+          <li className="listItems">Partner Help</li>
           <li className="listItems">Elite Membership</li>
           <li className="listItems">Return</li>
         </ul>
@@ -33,10 +33,9 @@ export const Footer = () => {
           <a href="" className="listTitle">
             Customer Service
           </a>
-          <li className="listItems">Careers</li>
-          <li className="listItems">Careers</li>
+          <li className="listItems">Investor Relations</li>
+          <li className="listItems">Press Center</li>
           <li className="listItems">Wrong Shipping</li>
-          <li className="listItems"></li>
         </ul>
       </div>
     </div>
