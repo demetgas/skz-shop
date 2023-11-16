1fe72b9da77c2ec50eb1a75abdf951f7804f328b
demetgas <demet_gas@hotmail.com>
2023-11-16 17:46:12 +0100

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,20 +11,32 @@ export const Footer = () => {
           </a>
           <li className="listItems">Frequently Asked Questions</li>
           <li className="listItems">Careers</li>
+          <li className="listItems">Careers</li>
         </ul>
         <ul className="list">
           <a href="" className="listTitle">
             ABOUT US
           </a>
           <li className="listItems">Contact Us</li>
+          <li className="listItems">Careers</li>
           <li className="listItems">Sustainability</li>
         </ul>
         <ul className="list">
           <a href="" className="listTitle">
             HELP
           </a>
+          <li className="listItems">Careers</li>
           <li className="listItems">Elite Membership</li>
           <li className="listItems">Return</li>
+        </ul>
+        <ul className="list">
+          <a href="" className="listTitle">
+            Customer Service
+          </a>
+          <li className="listItems">Careers</li>
+          <li className="listItems">Careers</li>
+          <li className="listItems">Wrong Shipping</li>
+          <li className="listItems"></li>
         </ul>
       </div>
     </div>
